3ec94e1236e4fd1a4b882618c6829788cdf9106b
MaksymHorelchyk <maksym.horelchyk@hys-enterprise.com>
2025-10-31 16:48:13 +0200

    feat (BE | tools) add NL2SQL tool

---
+++ src/shared/tools/nl2sql_tool/main.py
@@ -0,0 +1,41 @@
+# NL2SQL Tool
+
+import os
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_openai import ChatOpenAI
+
+class NL2SQLTool:
+    def __init__(
+        self,
+        db_uri: str,
+        openai_api_key: str | None = None
+    ):
+        self.db_uri = db_uri
+        self.openai_api_key = state["variables"]["OPENAI_API_KEY"] # Waiting for a fix
+
+    def _create_agent(self):
+
+        llm = ChatOpenAI(
+            model="gpt-4o-mini",
+            temperature=0,
+            api_key=self.openai_api_key
+        )
+        db = SQLDatabase.from_uri(self.db_uri)
+        agent_executor = create_sql_agent(
+            llm=llm,
+            db=db,
+            verbose=True
+        )
+        return agent_executor
+
+    def run_query(self, query_text: str) -> str:
+        agent = self._create_agent()
+        result = agent.invoke({"input": query_text})
+        return result["output"]
+
+def main(db_uri, query_text):
+    nl2sql = NL2SQLTool(db_uri)
+    result = nl2sql.run_query(query_text)
+    return result
+
