f1575da8a6a68ac26958e9b7229bb5f6652382fe
MaksymYevstratievHYS <maksym.yevstratiev@hys-enterprise.com>
2026-01-16 18:38:17 +0100

    fix(BE | realtime): fix realtime settings

--- src/realtime/run_server.py
+++ src/realtime/run_server.py
@@ -1,20 +1,20 @@
 import uvicorn
 from loguru import logger
-from .core.config import settings
+from core.config import settings
 
 
 def main():
-    if settings.DEBUG_MODE:
+    if settings.REALTIME_DEBUG_MODE:
         logger.info("RUNNING IN DEBUG MODE")
 
     uvicorn.run(
         "api.main:app",
         host="0.0.0.0",
-        port=settings.PORT,
-        reload=settings.RELOAD,
-        reload_dirs=["src"] if settings.RELOAD else None,
-        workers=settings.WORKERS,
-        log_level="debug" if settings.DEBUG_MODE else "info",
+        port=settings.REALTIME_PORT,
+        reload=settings.REALTIME_RELOAD,
+        reload_dirs=["src"] if settings.REALTIME_RELOAD else None,
+        workers=settings.REALTIME_WORKERS,
+        log_level="debug" if settings.REALTIME_DEBUG_MODE else "info",
     )
 
 
