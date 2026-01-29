1cea0b837f210ae55254f46d1e9b7ce25eeba201
sergey.tokarev <sergey.tokarev@hys-enterprice.com>
2026-01-29 21:59:01 +0100

    maintain: missed files

--- src/crew/models/request_models.py
+++ src/crew/models/request_models.py
@@ -110,7 +110,7 @@ class BaseToolData(BaseModel):
             raise ValueError(
                 "Invalid unique_name. Unique name should be splited by `:`. \nFor example: python-code-tool:1"
             )
-        if prefix == "python-code-tool" or "python-code-tool-config":
+        if prefix in {"python-code-tool", "python-code-tool-config"}:
             values["data"] = PythonCodeToolData(**data)
         elif prefix == "configured-tool":
             values["data"] = ConfiguredToolData(**data)
