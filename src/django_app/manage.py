2d007ad1429e8a9b01dbe487763893e69d38ac3b
ArturDrohomeretskyiHYS <artur.drohomeretskyi@hys-enterprise.com>
2026-02-04 19:07:31 +0200

    chore: log full errors

--- src/django_app/manage.py
+++ src/django_app/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from loguru import logger
 
 
 def main():
@@ -15,7 +16,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    try:
+        execute_from_command_line(sys.argv)
+    except Exception as e:
+        logger.exception(e)
 
 
 if __name__ == "__main__":
