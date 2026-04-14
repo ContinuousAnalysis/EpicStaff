1c515f42a14eebcbd280570ced596e34f59c5fb0
PavloNesterukHYS <pavlo.nesteruk@hys-enterprise.com>
2026-04-14 11:14:35 +0200

    feat(EST-2488): Update docs

--- src/django_app/tables/import_export/version_conversions/convertions.py
+++ src/django_app/tables/import_export/version_conversions/convertions.py
@@ -1,15 +1,11 @@
 # from tables.import_export.version_conversions.base import VersionConverter
 
-
-# # Example of concrete version converter
 # @VersionConverter.register(from_version=1)
 # def v1_to_v2(data: dict) -> dict:
 #     """
-#     Concrete import converter from v1 to v2
+#     Migrate from v1 to v2.
 
-#     (Optionally) Provide brief description of changes between versions
+#     (Describe what changed between versions.)
 #     """
-
-#     # convert logic
-
+#     # apply transformation
 #     return data
